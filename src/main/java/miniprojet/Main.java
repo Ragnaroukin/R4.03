cd1bfec1d209eb80794ecbb916d12f61a7efb288
Arthur POIRIER--DUBOIS <arthurpoidub@gmail.com>
2026-02-19 16:37:52 +0100

    [FEATURE] Insert Methods

--- src/main/java/miniprojet/Main.java
+++ src/main/java/miniprojet/Main.java
@@ -1,10 +1,41 @@
 package miniprojet;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import miniprojet.jdbc.JDBC;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (Connection conn = DriverManager.getConnection(JDBC.getInstance().getUrl(), JDBC.getInstance().getUser(), JDBC.getInstance().getPassword())) {
 
+            System.out.println("Connexion réussie !");
+
+            Statement st = conn.createStatement();
+
+            st.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS test (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100))"
+            );
+
+            st.executeUpdate("INSERT INTO test(name) VALUES ('Arthur')");
+
+            ResultSet rs = st.executeQuery("SELECT * FROM test");
+
+            while (rs.next()) {
+                System.out.println(
+                    rs.getInt("id") + " - " +
+                    rs.getString("name")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
-
 }
