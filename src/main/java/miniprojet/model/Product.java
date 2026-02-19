cdac9ee8baa1ee54dbe1662fadba5e6911a27eb7
Arthur POIRIER--DUBOIS <arthurpoidub@gmail.com>
2026-02-19 17:00:17 +0100

    [FEATURE] Product model

---
+++ src/main/java/miniprojet/model/Product.java
@@ -0,0 +1,42 @@
+package miniprojet.model;
+
+public class Product {
+
+    private int id;
+    private String nom;
+    private double prix;
+    private int quantite;
+
+    public Product(int id, String nom, double prix, int quantite) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                ", quantite=" + quantite +
+                '}';
+    }
+}
