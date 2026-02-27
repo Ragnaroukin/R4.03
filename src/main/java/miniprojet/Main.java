bb786c4d17379a10abdb9636c19136ce0832d3be
Arthur POIRIER--DUBOIS <ragnaroukin@gmail.com>
2026-02-27 07:34:12 +0100

    [FEATURE] Modify and verify methods

--- src/main/java/miniprojet/Main.java
+++ src/main/java/miniprojet/Main.java
@@ -11,31 +11,5 @@ import miniprojet.jdbc.JDBC;
 public class Main {
 
 	public static void main(String[] args) {
-		try (Connection conn = DriverManager.getConnection(JDBC.getInstance().getUrl(), JDBC.getInstance().getUser(), JDBC.getInstance().getPassword())) {
-
-            System.out.println("Connexion réussie !");
-
-            Statement st = conn.createStatement();
-
-            st.executeUpdate(
-                "CREATE TABLE IF NOT EXISTS test (" +
-                "id INT AUTO_INCREMENT PRIMARY KEY," +
-                "name VARCHAR(100))"
-            );
-
-            st.executeUpdate("INSERT INTO test(name) VALUES ('Arthur')");
-
-            ResultSet rs = st.executeQuery("SELECT * FROM test");
-
-            while (rs.next()) {
-                System.out.println(
-                    rs.getInt("id") + " - " +
-                    rs.getString("name")
-                );
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 	}
 }
